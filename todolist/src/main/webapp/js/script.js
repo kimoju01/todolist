eebb4b25ac3945fb8d2a6c2ef0ceee1b19e59e23
kimoju01 <kimoju03@naver.com>
2020-10-30 21:30:29 +0900

    feat: toggleButtonChange() 함수 추가
    * checkbox 클릭 시, ajax 통신으로 해당하는 항목의 타입을 변경해준다.
    - doing -> done 으로 갈 때, 우선순위는 2로 변경된다.

--- todolist/src/main/webapp/js/script.js
+++ todolist/src/main/webapp/js/script.js
@@ -1,97 +1,77 @@
+function check() {
+	var form = document.writetask;
 
-var doingList = document.querySelector('.doing-list');
-var doneList = document.querySelector('.done-list');
+	if (form.newtask.value == "") {
+		alert("할 일을 적어주세요");
+		form.newtask.focus();
+		return false;
+	}
 
+	form.submit();
 
-loadEventListners();
-
-function loadEventListners() {
-    document.addEventListener('change', toggleItem);
-    document.addEventListener('click', removeItem); 
-    document.addEventListener('click', starItem);   
 }
 
-function addNewItem(itemText) {
-    var li = document.createElement('li');
-    var div = document.createElement('div');
-    div.setAttribute('class', 'view');
+function destroyButtonClick(id, eventNode) {
 
-    var star = document.createElement('input');
-    star.setAttribute('class', 'star');
-    star.setAttribute('type', 'button');
-    star.setAttribute('value', '★');
-    // star.setAttribute('checked', 'false');
+	var httpRequest = new XMLHttpRequest();
+	httpRequest.addEventListener("load", function() {
+		if (this.responseText === "success")
+			eventNode.parentNode.parentNode.remove();
+	});
 
-    var input = document.createElement('input');
-    input.setAttribute('class', 'toggle');
-    input.setAttribute('type', 'checkbox');
-
-    var label = document.createElement('label');
-
-    var btn = document.createElement('button');
-    btn.setAttribute('class', 'destroy');
-
-    li.append(div);
-    div.append(star);
-    div.append(input);
-    div.append(label);
-    div.appendChild(btn);
-
-    label.innerText = itemText;
-
-    doingList.appendChild(li);
-
-    inputText.value = "";
-    }
-
-function removeItem(e) {
-    if(e.target && e.target.className == 'destroy') {
-        e.target.parentElement.parentElement.remove();
-    }
+	httpRequest.open('GET', "./delete?id=" + id);
+	httpRequest.send();
 }
 
-function toggleItem(e) {
-    if(e.target && e.target.className == 'toggle') {
-        if(e.target.parentElement.parentElement.parentElement == doingList) {
-            e.target.previousSibling.remove();
-            doneList.appendChild(e.target.parentElement.parentElement);
-            e.target.outerHTML = '<input class="toggle" type="checkbox" checked></input>';
-           } else if(e.target.parentElement.parentElement.parentElement == doneList) {
-            doingList.appendChild(e.target.parentElement.parentElement);
-            var star = document.createElement('input');
-            star.setAttribute('class', 'star');
-            star.setAttribute('type', 'button');
-            star.setAttribute('value', '★');
-            // star.setAttribute('checked', 'false');
-            e.target.parentElement.insertBefore(star, e.target);
-            e.target.outerHTML = '<input class="toggle" type="checkbox"></input>';
-        }
-    }
+function starButtonClick(id, sequence, eventNode) {
+
+	var httpRequest = new XMLHttpRequest();
+	httpRequest.addEventListener("load", function() {
+		if (this.responseText === "success") {
+			if (sequence == 1) {
+				eventNode.style.color = "rgb(200, 200, 200)";
+				sequence = 2;
+			} else if (sequence == 2) {
+				eventNode.style.color = "orange";
+				sequence = 1;
+			}
+		}
+	});
+
+	httpRequest.open('GET', "./sequence?id=" + id + "&sequence=" + sequence);
+	httpRequest.send();
+
 }
 
-function starItem(e) {
-    if(e.target && e.target.className == 'star') {
-        if(e.target.checked) {
-            e.target.style.color = "rgb(200, 200, 200)";
-            e.target.checked = false;
-        } else {
-            e.target.style.color = "orange";
-            doingList.insertBefore(e.target.parentElement.parentElement, doingList.firstChild);
-            e.target.checked = true;
-        }
-    }
+function toggleButtonChange(id, sequence, eventNode) {
+
+	var httpRequest = new XMLHttpRequest();
+	var type = eventNode.id;
+	var insertItem = eventNode.parentNode.parentNode;
+
+	httpRequest.addEventListener("load", function() {
+		if (this.responseText === "success") {
+			if (type == 'DOING') {
+				type = "DONE";
+				eventNode.previousSibling.previousSibling.remove();
+				eventNode.outerHTML = '<input class="toggle" id="${todo.type }" type="checkbox" onchange="toggleButtonChange(${todo.id}, ${todo.type}, this)" checked></input>';
+			} else if (type == 'DONE') {
+				type = "DOING";
+
+				var star = document.createElement('input');
+				star.setAttribute('class', 'star');
+				star.setAttribute('type', 'button');
+				star.setAttribute('value', '★ ');
+				eventNode.parentNode.insertBefore(star,eventNode);
+				eventNode.outerHTML = '<input class="toggle" type="checkbox"></input>';
+				}
+			}
+
+			var list = document.querySelector("ul[id=" + type + "]");
+			list.appendChild(insertItem);
+		});
+
+	httpRequest.open('GET', "./type?id=" + id + "&type=" + type + "&sequence=" + sequence);
+	httpRequest.send();
+
 }
-
-var inputText = document.querySelector('.new-todo');
-inputText.addEventListener('keypress', function(e) {
-    if(e.which === 13) {
-        var itemText = inputText.value;
-        if(!itemText || itemText === "" || itemText === " ") return false;
-          
-        addNewItem(itemText);
-        }
-});
-
-
-
-
